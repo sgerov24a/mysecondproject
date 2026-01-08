df22113fab848a21b9f5d2383cab831dfecae114
sgerov24a <sgerov24a@gmail.com>
2026-01-08 10:45:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -29,8 +29,7 @@ def load_model():
     max_iter=100,
     random_state=42
 )
-model.fit(X_train, y_train)
-return model
+
 except Exception as e:
     st.error(f"Model loading error: {e}")
     return None
